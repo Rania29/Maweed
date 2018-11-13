54f51e685daee26cce4ac0004a4f46346045c26b
samir82show <samir.ahmedmustafa@gmail.com>
2018-11-13 23:08:16 +0300

    update - Tue 11/13/2018 23:08:15.95

--- src/java/facade/HospitalFacade.java
+++ src/java/facade/HospitalFacade.java
@@ -10,10 +10,6 @@ import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
-/**
- *
- * @author sawad
- */
 @Stateless
 public class HospitalFacade extends AbstractFacade<Hospital> {
 
